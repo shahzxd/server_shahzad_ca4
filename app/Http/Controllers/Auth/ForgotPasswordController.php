2416fe505eab58d44597eabd68fe2f6498bf7062
shahzxd <shahzadshabeer13@gmail.com>
2024-05-13 08:31:04 +0100

    added forgotpassword controller

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    /*
+    Password Reset Controller
+    */
+
+    use SendsPasswordResetEmails;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+}
